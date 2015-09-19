6e0e0b1df38582b8e9a0d7cfa952a2ba6445c6c1
Oliver Schumacher <oschumac@users.noreply.github.com>
2015-09-19 12:13:22 +0200

    Update pebble.sh

--- bin/pebble.sh
+++ bin/pebble.sh
@@ -5,13 +5,24 @@ cd ~/openaps-dev
 stat -c %y clock.json | cut -c 1-19
 cat clock.json | sed 's/"//g' | sed 's/T/ /'
 echo
-#calculate-iob pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && mv iob.json.new iob.json
-node ~/openaps-js/bin/iob.js pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && cp iob.json.new iob.json #&& git commit -m"iob found: committing" iob.json
-#determine-basal iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep temp requestedtemp.json.new && mv requestedtemp.json.new requestedtemp.json
-node ~/openaps-js/bin/determine-basal.js iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep temp requestedtemp.json.new && cp requestedtemp.json.new requestedtemp.json #&& git commit -m"temp found: committing" requestedtemp.json
-#git fetch origin master && git merge -X theirs origin/master && git push
-#git pull && git push
-node ~/openaps-js/bin/pebble.js  glucose.json clock.json iob.json current_basal_profile.json currenttemp.json isf.json requestedtemp.json > /tmp/pebble-openaps.json
-cat /tmp/pebble-openaps.json
-grep "refresh_frequency" /tmp/pebble-openaps.json && cp /tmp/pebble-openaps.json www/openaps.json 
-cat www/openaps.json
+
+# aktuelle Temp lesen nach änderung vom Prozess
+openaps report invoke currenttemp.json
+
+nodejs /home/pi/openaps-dev/pebble.js  glucose.json clock.json iob.json current_basal_profile.json currenttemp.json isf.json requestedtemp.json profile.json > www/openaps.json
+
+
+
+IP_address=cgmcloud.de
+username="pebbleupload"
+PASSWD=gVh3e6_7
+USER=pebbleupload
+FILE='www/openaps.json openaps.json'
+
+ftp -n $IP_address <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+put $FILE
+bye
+END_SCRIPT
+
